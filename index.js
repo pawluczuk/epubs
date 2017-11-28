0734ac363812e59ad519927d339339cccba6101f
pawluczuk <pawluczuk.monika@gmail.com>
2017-11-28 10:28:24 +0000

    added comments

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ class Extract {
         this.dir = dir;
     }
 
+    /**
+     * Formats metadata (title and contributors)
+     * @param {Object} metadata 
+     */
     formatMetadata(metadata) {
         if (!metadata || !metadata.title || !metadata.creator) {
             return undefined;
@@ -18,6 +22,12 @@ class Extract {
         };
     }
 
+    /**
+     * Saves metadata file. Creates index.json file in a folder matching name of the epub file
+     * @param {String} file 
+     * @param {Object} metadata 
+     * @param {Function} cb 
+     */
     writeMetadata(file, metadata, cb) {
         const folderName = file.replace(/\.epub$/i, ''),
             folderPath = path.resolve(this.dir, folderName);
@@ -36,6 +46,13 @@ class Extract {
         });
     }
 
+    /**
+     * Reads the parsed epub metadata, formats it and saves it
+     * @param {Object} epub 
+     * @param {String} file 
+     * @param {Function} resolve 
+     * @param {Function} reject 
+     */
     saveMetadata(epub, file, resolve, reject) {
         const metadata = this.formatMetadata(epub.metadata);
         
@@ -64,13 +81,18 @@ class Extract {
         });
     }
 
+    /**
+     * Parses epub with a given path and saves its metadata 
+     * @param {String} file 
+     */
     extractMetadata(file) {
         return new Promise((resolve, reject) => {
             file = path.resolve(this.dir, file);
             const epub = new EPub(file);
 
             epub
-                .on('end', () => {                   
+                .on('end', () => { 
+                    // epub was parsed successfully                  
                     return this.saveMetadata(epub, file, resolve, reject);
                 })
                 .on('error', err => {
@@ -85,17 +107,25 @@ class Extract {
             epub.parse();
         });
     }
-
+    /**
+     * Maps array of files to array of Promises that resolve to extract file's metadata
+     * @param {Array<String>} files 
+     */
     getAllMetadata(files) {
         return files
             .filter(file => {
                 return /.+\.epub$/i.test(file);
             })
             .map(file => {
+                // catches the error so that single file processing won't stop all files execution
                 return this.extractMetadata(file).catch(e => e);
             });
     }
 
+    /**
+     * Processes all epub files in a given directory 
+     * and saves their metadata in a created folder with name of the book
+     */
     extractAllFiles() {
         return new Promise((resolve, reject) => {
             fs.readdir(this.dir, (err, files) => {
@@ -104,7 +134,7 @@ class Extract {
                 }
 
                 const promises = this.getAllMetadata(files);
-                console.log(promises)
+                // return when all files have been processed
                 return Promise.all(promises)
                     .then(data => {
                         resolve(data);
